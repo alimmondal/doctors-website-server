8f5e8617591869869fdb716d869baaa0bb98ae89
alimmondal <italimbd@gmail.com>
2021-07-14 20:35:46 +0600

    updated

--- index.js
+++ index.js
@@ -105,6 +105,20 @@ app.post('/isAdmins', (req, res) => {
 });
 
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like main.js or main.css
+    app.use(express.static('client/build'));
+  
+    // Express will serve up the index.html file if
+    // it doesnt recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
+
 app.get('/', (req, res) => {
   res.send("Hello from db, it's working working")
 })
